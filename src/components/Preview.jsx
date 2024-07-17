54d5a49fd64b10ca21e0403e2fdbac93d9760991
fathyElgazar <gazzarDev@gmail.com>
2024-07-17 16:41:24 +0300

    feat: Add preview experience

--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -3,10 +3,53 @@ import { MdOutlineLocationOn } from 'react-icons/md';
 import { MdMailOutline } from 'react-icons/md';
 import '../styles/preview.css';
 
+const Info = ({
+  institution,
+  className,
+  position,
+  rule,
+  degree,
+  dateRange,
+  location,
+  summary,
+}) => {
+  return (
+    <>
+      <div className={className}>
+        <h2 className='section-header'>
+          {className === 'education-info' ? 'Education' : 'Experience'}
+        </h2>
+        <h3 className='institution-name'>{institution ? institution : ''}</h3>
+        <p className='rule'>{rule ? rule : position}</p>
+        <p className='date'>{dateRange ? dateRange : ''} </p>
+        <p className={degree ? 'location ' : 'align-location'}>
+          {location ? (
+            <>
+              <MdOutlineLocationOn /> {location}
+            </>
+          ) : (
+            ''
+          )}
+        </p>
+        <p className='degree'>{degree ? degree : ''} </p>
+        {summary ? (
+          <ul className='summary'>
+            <li>{summary ? summary : ''}</li>
+          </ul>
+        ) : (
+          ''
+        )}
+      </div>
+    </>
+  );
+};
+
 const Preview = ({ personalInfo, educationInfo, experienceInfo }) => {
   const { name, headline, email, phone, address } = personalInfo;
   const { institution, areaOfStudy, degree, dateRange, location, summary } =
     educationInfo;
+  const { company, position, companyDateRange, jobSummary, jobLocation } =
+    experienceInfo;
   return (
     <>
       <div className='preview'>
@@ -45,29 +88,23 @@ const Preview = ({ personalInfo, educationInfo, experienceInfo }) => {
             </p>
           </div>
         </div>
-        <div className='education-info'>
-          <h2 className='education-header'>Education</h2>
-          <h3 className='institution-name'>{institution ? institution : ''}</h3>
-          <p className='area-of-study'>{areaOfStudy ? areaOfStudy : ''}</p>
-          <p className='date'>{dateRange ? dateRange : ''} </p>
-          <p className='location'>
-            {location ? (
-              <>
-                <MdOutlineLocationOn /> {location}
-              </>
-            ) : (
-              ''
-            )}
-          </p>
-          <p className='degree'>{degree ? degree : ''} </p>
-          {summary ? (
-            <ul className='summary'>
-              <li>{summary ? summary : ''}</li>
-            </ul>
-          ) : (
-            ''
-          )}
-        </div>
+        <Info
+          className='education-info'
+          institution={institution}
+          degree={degree}
+          dateRange={dateRange}
+          summary={summary}
+          location={location}
+          rule={areaOfStudy}
+        />
+        <Info
+          className='experience-info'
+          institution={company}
+          dateRange={companyDateRange}
+          summary={jobSummary}
+          location={jobLocation}
+          rule={position}
+        />
       </div>
     </>
   );
